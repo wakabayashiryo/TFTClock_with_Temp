9fdeea4323ef9bd71e443a3e59dbafd6b377f0b9
EVAOTA <nervevawaka@ezweb.ne.jp>
2017-11-05 10:34:41 +0900

    confirme behaver after previous commit

--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -160,34 +160,6 @@ void USART2_IRQHandler(void)
 }
 
 /**
-* @brief This function handles I2C1 event interrupt.
-*/
-void I2C1_EV_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_EV_IRQn 0 */
-
-  /* USER CODE END I2C1_EV_IRQn 0 */
-  HAL_I2C_EV_IRQHandler(&hi2c1);
-  /* USER CODE BEGIN I2C1_EV_IRQn 1 */
-
-  /* USER CODE END I2C1_EV_IRQn 1 */
-}
-
-/**
-* @brief This function handles I2C1 error interrupt.
-*/
-void I2C1_ER_IRQHandler(void)
-{
-  /* USER CODE BEGIN I2C1_ER_IRQn 0 */
-
-  /* USER CODE END I2C1_ER_IRQn 0 */
-  HAL_I2C_ER_IRQHandler(&hi2c1);
-  /* USER CODE BEGIN I2C1_ER_IRQn 1 */
-
-  /* USER CODE END I2C1_ER_IRQn 1 */
-}
-
-/**
 * @brief This function handles DMA1 stream7 global interrupt.
 */
 void DMA1_Stream7_IRQHandler(void)
