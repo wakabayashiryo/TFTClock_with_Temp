5427e453eac0f35487959cc9a4ba62f7601fab11
EVAOTA <nervevawaka@ezweb.ne.jp>
2018-01-21 23:25:31 +0900

    add backlight control pin

--- Src/main.c
+++ Src/main.c
@@ -483,7 +483,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, LCD_RST_Pin|LCD_D1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, BackLight_Pin|LCD_RST_Pin|LCD_D1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, LCD_RD_Pin|LCD_WR_Pin|LCD_RS_Pin|LCD_D7_Pin 
@@ -499,8 +499,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LCD_RST_Pin LCD_D1_Pin */
-  GPIO_InitStruct.Pin = LCD_RST_Pin|LCD_D1_Pin;
+  /*Configure GPIO pins : BackLight_Pin LCD_RST_Pin LCD_D1_Pin */
+  GPIO_InitStruct.Pin = BackLight_Pin|LCD_RST_Pin|LCD_D1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
